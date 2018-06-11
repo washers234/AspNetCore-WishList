9a685bee821c53231accee4d83132d063cf1f6ba
washers234 <38015496+washers234@users.noreply.github.com>
2018-06-11 21:26:00 +0100

    Create Model

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items;
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
+            
         }
 
     }
