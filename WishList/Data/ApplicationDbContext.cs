a4c609ca1c231f54614ec5f38d4532dd8734a9a7
washers234 <38015496+washers234@users.noreply.github.com>
2018-06-11 21:08:23 +0100

    appdb class

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
